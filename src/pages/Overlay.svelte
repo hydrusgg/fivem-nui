5e5620714ad2129898f8d951e89c1ec2cbeb230a
jesteriruka <jesteriruka@gmail.com>
2024-01-10 15:57:53 -0300

    feat: getStoreCommand callback

--- src/pages/Overlay.svelte
+++ src/pages/Overlay.svelte
@@ -1,15 +1,21 @@
 <script>
   import { fade } from "svelte/transition";
+  import { post } from "../lib/api";
+  import { writable } from "svelte/store";
+
+  let command = writable("store");
+
+  post("getStoreCommand").then(command.set, () => {});
 </script>
 
 <div transition:fade id="overlay">
   <header>
     <h1>BENEFÍCIOS</h1>
   </header>
-  
+
   <p>Você possui benefícios pendentes</p>
-  
+
   <footer>
-    <p>Utilize /store para resgatar</p>
+    <p>Utilize /{$command} para resgatar</p>
   </footer>
 </div>
